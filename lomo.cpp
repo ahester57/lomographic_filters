ebfd9d69dbbf9ff224112b22121b6cde62e083df
Austin Hester <ahester@incomm.com>
2020-11-11 19:33:46 -0600

    add second trackbar
    Signed-off-by: Austin Hester <ahester@incomm.com>

--- lomo.cpp
+++ lomo.cpp
@@ -14,7 +14,9 @@
 #include "./include/histo_func.hpp"
 
 
-int slider_value = 0;
+const std::string window_name = "Lomography";
+int slider_red_value = 0;
+int slider_vig_value = 0;
 cv::Mat original_image;
 cv::Mat blended_image;
 
@@ -31,12 +33,22 @@ wait_key()
 }
 
 static void
-on_trackbar1(int, void*)
+on_trackbar_red_level(int, void*)
 {
-    double alpha = (double) slider_value / 12;
+    double alpha = (double) slider_red_value / 12;
     double beta = 1.0 - alpha;
     original_image = run_image_matching(original_image, blended_image);
-    cv::imshow("original", original_image);
+    cv::imshow(window_name, original_image);
+    blended_image = original_image;
+}
+
+static void
+on_trackbar_vignette(int, void*)
+{
+    double alpha = (double) slider_vig_value / 100;
+    double beta = 1.0 - alpha;
+    original_image = run_image_matching(original_image, blended_image);
+    cv::imshow(window_name, original_image);
     blended_image = original_image;
 }
 
@@ -63,9 +75,10 @@ main(int argc, const char** argv)
     blended_image = open_image("bear.jpg", true)->image;
     original_image = og_image->image;
     // display the original image
-    cv::imshow("original", original_image);
+    cv::imshow(window_name, original_image);
 
-    cv::createTrackbar("trackbar1", "original", &slider_value, 12, on_trackbar1);
+    cv::createTrackbar("Red Level", window_name, &slider_red_value, 12, on_trackbar_red_level);
+    cv::createTrackbar("Vignette", window_name, &slider_vig_value, 100, on_trackbar_vignette);
 
     wait_key();
 
